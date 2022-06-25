a1823df3f2dcb7d37ab13771e2909da233f4aa12
jackloague1 <55412612+jackloague1@users.noreply.github.com>
2022-06-25 14:09:49 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset = "UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Website</title>
+</head>
+
+<h1> University Login </h1>
+<body>
+    <div><h2>University Login</h2></div>
+    <form action='connect.php' method="POST">
+        <label for="user">SSN:</label><br>
+        <input type='text' name='name' id="name" required/> <br> <br>
+        <input type='submit' name='submit' id='submit'/>
+    </form>
+</html>
